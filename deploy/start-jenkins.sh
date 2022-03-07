fadc2077bbbc723c333bb5faeb04dd0c13ae2ebc
dipti1986 <hello.diptisingh@gmail.com>
2022-03-07 13:49:32 +0000

    fix: DIP-9 removed db files

--- deploy/start-jenkins.sh
+++
@@ -1,3 +0,0 @@
-docker run -d -p 8080:8080 \
-    -v /Users/sanchoud1/Documents/Dipti/jenkins_home:/var/jenkins_home \
-    jenkins/jenkins
--- deploy/start-manage-user-app.sh
+++ deploy/start-manage-user-app.sh
@@ -3,4 +3,4 @@ docker run -d -p 5000:5000 \
     -e MYSQL_DATABASE_PASSWORD=password \
     -e MYSQL_DATABASE_DB=userdb \
     -e MYSQL_DATABASE_HOST=host.docker.internal \
-    manage-user:0.1 
+    diptichoudhary/manage-user:0.1
