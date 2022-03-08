e27e4f776a2308a361479daa3d868449bb143fc6
dipti1986 <hello.diptisingh@gmail.com>
2022-03-08 02:07:09 +0000

    feat: DIP-9 Added code coverage

--- deploy/start-manage-user-db.sh
+++ deploy/start-manage-user-db.sh
@@ -1,4 +1,4 @@
-docker run -d -p 3306:3306 \
+sudo docker run -d -p 3306:3306 \
     -e MYSQL_DATABASE=userdb \
     -e MYSQL_ROOT_PASSWORD=password \
     -v $(pwd)/create_db.sql:/docker-entrypoint-initdb.d/1.sql \
